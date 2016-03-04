134ecaea04012229748d7847359caaa0f122cd44
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-03-04 14:22:10 +0100

    Implemented `write` for Android

--- bluetooth.android.js
+++ bluetooth.android.js
@@ -55,7 +55,7 @@ Bluetooth._connections = {};
 (function () {
   var bluetoothManager = utils.ad.getApplicationContext().getSystemService(android.content.Context.BLUETOOTH_SERVICE);
   adapter = bluetoothManager.getAdapter();
-  
+
   if (android.os.Build.VERSION.SDK_INT >= 21 /*android.os.Build.VERSION_CODES.LOLLIPOP */) {
     var MyScanCallback = android.bluetooth.le.ScanCallback.extend({
       onBatchScanResults: function(results) {
@@ -73,7 +73,7 @@ Bluetooth._connections = {};
         } else if (errorCode == android.bluetooth.le.ScanCallback.SCAN_FAILED_INTERNAL_ERROR) {
           errorMessage = "Internal error";
         } else {
-          errorMessage = "Scan failed to start";          
+          errorMessage = "Scan failed to start";
         }
         console.log("------- scanCallback.onScanFailed errorMessage: " + errorMessage);
       },
@@ -122,7 +122,7 @@ Bluetooth._connections = {};
 // callback for connecting and read/write operations
 Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
   // add constructor which gets a callback
-  
+
   /**
    * newState (BluetoothProfile.state)
    * 0: disconnected
@@ -142,7 +142,7 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
       Bluetooth._disconnect(bluetoothGatt);
     }
   },
-  
+
   onServicesDiscovered: function(bluetoothGatt, status) {
     console.log("------- _MyGattCallback.onServicesDiscovered, status (0=success): " + status);
 
@@ -181,7 +181,7 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
             }
             descriptorsJs.push(descriptorJs);
           }
-          
+
           var characteristicJs = {
             UUID: Bluetooth._uuidToString(characteristic.getUuid()),
             name: Bluetooth._uuidToString(characteristic.getUuid()), // there's no sep field on Android
@@ -229,7 +229,21 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
       });
     }
   },
-  
+
+  _decodeValue: function(value) {
+    if (value === null) {
+      return null;
+    }
+    // TODO may be Uint16Array as well
+    var data = new Uint8Array(value);
+    for (var d in data) {
+      if (data[d] !== undefined) {
+        return data[d];
+      }
+    }
+    return value;
+  },
+
   onCharacteristicRead: function(bluetoothGatt, bluetoothGattCharacteristic, status) {
     console.log("------- _MyGattCallback.onCharacteristicRead");
 
@@ -242,15 +256,14 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
 
     if (stateObject.onReadPromise) {
       var value = bluetoothGattCharacteristic.getValue();
-      var data = new Uint8Array(value);
       stateObject.onReadPromise({
         value: value,
-        valueDecoded: data[1],
+        valueDecoded: this._decodeValue(value),
         characteristicUUID: bluetoothGattCharacteristic.getUuid()
       });
     }
   },
-  
+
   onCharacteristicChanged: function(bluetoothGatt, bluetoothGattCharacteristic) {
     console.log("------- _MyGattCallback.onCharacteristicChanged");
 
@@ -260,7 +273,7 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
       Bluetooth._disconnect(bluetoothGatt);
       return;
     }
-    
+
     if (stateObject.onNotifyCallback) {
       var value = bluetoothGattCharacteristic.getValue();
       var data = new Uint8Array(value);
@@ -272,16 +285,30 @@ Bluetooth._MyGattCallback = android.bluetooth.BluetoothGattCallback.extend({
       });
     }
   },
-  
+
   onCharacteristicWrite: function(bluetoothGatt, bluetoothGattCharacteristic, status) {
     console.log("------- _MyGattCallback.onCharacteristicWrite");
-    // TODO implement
+
+    var device = bluetoothGatt.getDevice();
+    var stateObject = Bluetooth._connections[device.getAddress()];
+    if (!stateObject) {
+      Bluetooth._disconnect(bluetoothGatt);
+      return;
+    }
+
+    console.log(bluetoothGattCharacteristic);
+
+    if (stateObject.onWritePromise) {
+      stateObject.onWritePromise({
+        characteristicUUID: bluetoothGattCharacteristic.getUuid()
+      });
+    }
   },
-  
+
   onDescriptorRead: function(bluetoothGatt, bluetoothGattDescriptor, status) {
     console.log("------- _MyGattCallback.onDescriptorRead");
   },
-  
+
   onDescriptorWrite: function(bluetoothGatt, bluetoothGattDescriptor, status) {
     console.log("------- _MyGattCallback.onDescriptorWrite");
   },
@@ -345,7 +372,7 @@ Bluetooth.startScanning = function (arg) {
       for (var s in serviceUUIDs) {
         uuids.push(Bluetooth._stringToUuid(serviceUUIDs[s]));
       }
-      
+
       if (android.os.Build.VERSION.SDK_INT < 21 /*android.os.Build.VERSION_CODES.LOLLIPOP */) {
         var didStart = uuids.length === 0 ?
             adapter.startLeScan(Bluetooth._scanCallback) :
@@ -372,14 +399,14 @@ Bluetooth.startScanning = function (arg) {
 
         var scanMode = arg.scanMode || android.bluetooth.le.ScanSettings.SCAN_MODE_LOW_LATENCY;
         scanSettings.setScanMode(scanMode);
-        
+
         if (android.os.Build.VERSION.SDK_INT >= 23 /*android.os.Build.VERSION_CODES.M */) {
           var matchMode = arg.matchMode || android.bluetooth.le.ScanSettings.MATCH_MODE_AGGRESSIVE;
           scanSettings.setMatchMode(matchMode);
 
           var matchNum = arg.matchNum || android.bluetooth.le.ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT;
           scanSettings.setNumOfMatches(matchNum);
-          
+
           var callbackType = arg.callbackType || android.bluetooth.le.ScanSettings.CALLBACK_TYPE_ALL_MATCHES;
           scanSettings.setCallbackType(callbackType);
         }
@@ -439,7 +466,7 @@ Bluetooth._disconnect = function(gatt) {
         name: device.getName()
       });
     } else {
-      console.log("----- !!! no disconnect callback found");      
+      console.log("----- !!! no disconnect callback found");
     }
     Bluetooth._connections[device.getAddress()] = null;
     gatt.close();
@@ -461,9 +488,9 @@ Bluetooth.connect = function (arg) {
       } else {
         console.log("Connecting to device with UUID: " + arg.UUID);
         var bluetoothGatt = bluetoothDevice.connectGatt(
-          utils.ad.getApplicationContext(), // context
-          false, // autoconnect
-          new Bluetooth._MyGattCallback(/* TODO pass in onWhatever function */)
+            utils.ad.getApplicationContext(), // context
+            false, // autoconnect
+            new Bluetooth._MyGattCallback(/* TODO pass in onWhatever function */)
         );
         // TODO (fill, and use this object in disconnect() as well)
         Bluetooth._connections[arg.UUID] = {
@@ -493,7 +520,7 @@ Bluetooth.disconnect = function (arg) {
         reject("Device wasn't connected");
         return;
       }
-      
+
       Bluetooth._disconnect(connection.device);
       resolve();
     } catch (ex) {
@@ -510,36 +537,34 @@ Bluetooth._findNotifyCharacteristic = function(bluetoothGattService, characteris
   for (var i = 0; i < characteristics.size(); i++) {
     var c = characteristics.get(i);
     if ((c.getProperties() & android.bluetooth.BluetoothGattCharacteristic.PROPERTY_NOTIFY) !== 0 && characteristicUUID.equals(c.getUuid())) {
-        return c;
+      return c;
     }
   }
-  
+
   // If there wasn't a Notify Characteristic, check for Indicate
   for (i = 0; i < characteristics.size(); i++) {
     var ch = characteristics.get(i);
     if ((ch.getProperties() & android.bluetooth.BluetoothGattCharacteristic.PROPERTY_INDICATE) !== 0 && characteristicUUID.equals(ch.getUuid())) {
-        return ch;
+      return ch;
     }
   }
-  
+
   // As a last resort, try and find ANY characteristic with this UUID, even if it doesn't have the correct properties
   return bluetoothGattService.getCharacteristic(characteristicUUID);
-};      
+};
 
 // This guards against devices reusing char UUID's.
-Bluetooth._findReadableCharacteristic = function(bluetoothGattService, characteristicUUID) {
-
+Bluetooth._findCharacteristicOfType = function(bluetoothGattService, characteristicUUID, charType) {
   var characteristics = bluetoothGattService.getCharacteristics();
   for (var i = 0; i < characteristics.size(); i++) {
     var c = characteristics.get(i);
-    if ((c.getProperties() & android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ) !== 0 && characteristicUUID.equals(c.getUuid())) {
-        return c;
+    if ((c.getProperties() & charType) !== 0 && characteristicUUID.equals(c.getUuid())) {
+      return c;
     }
   }
-  
   // As a last resort, try and find ANY characteristic with this UUID, even if it doesn't have the correct properties
   return bluetoothGattService.getCharacteristic(characteristicUUID);
-};      
+};
 
 Bluetooth._getWrapper = function (arg, reject) {
   if (!Bluetooth._isEnabled()) {
@@ -595,16 +620,15 @@ Bluetooth.read = function (arg) {
       var bluetoothGattService = wrapper.bluetoothGattService;
       var characteristicUUID = Bluetooth._stringToUuid(arg.characteristicUUID);
 
-      var bluetoothGattCharacteristic = Bluetooth._findReadableCharacteristic(bluetoothGattService, characteristicUUID);
+      var bluetoothGattCharacteristic = Bluetooth._findCharacteristicOfType(bluetoothGattService, characteristicUUID, android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ);
       if (!bluetoothGattCharacteristic) {
         reject("Could not find characteristic with UUID " + arg.characteristicUUID + " on service with UUID " + arg.serviceUUID + " on device with UUID " + arg.deviceUUID);
         return;
       }
 
-      if (gatt.readCharacteristic(bluetoothGattCharacteristic)) {
-        var stateObject = Bluetooth._connections[arg.deviceUUID];
-        stateObject.onReadPromise = resolve;
-      } else {
+      var stateObject = Bluetooth._connections[arg.deviceUUID];
+      stateObject.onReadPromise = resolve;
+      if (!gatt.readCharacteristic(bluetoothGattCharacteristic)) {
         reject("Failed to set client characteristic read for " + characteristicUUID);
       }
     } catch (ex) {
@@ -614,11 +638,56 @@ Bluetooth.read = function (arg) {
   });
 };
 
+// val must be a Uint8Array or Uint16Array or a string like '0x01' or '0x007F' or '0x01,0x02', or '0x007F,'0x006F''
+Bluetooth._encodeValue = function(val) {
+  // if it's not a string assume it's a byte array already
+  if (typeof val != 'string') {
+    return val;
+  }
+  var parts = val.split(',');
+  if (parts[0].indexOf('x') == -1) {
+    return null;
+  }
+  var result = Array.create("byte", parts.length);
+
+  for (var i=0; i<parts.length; i++) {
+    result[i] = parts[i];
+  }
+  return result;
+};
+
 Bluetooth.write = function (arg) {
   return new Promise(function (resolve, reject) {
     try {
-      // TODO implement
-      reject("write is not yet implemented for Android");
+      if (!arg.value) {
+        reject("You need to provide some data to write in the 'value' property");
+        return;
+      }
+      var wrapper = Bluetooth._getWrapper(arg, reject);
+      if (wrapper === null) {
+        // no need to reject, this has already been done
+        return;
+      }
+
+      var bluetoothGattCharacteristic = Bluetooth._findCharacteristicOfType(wrapper.bluetoothGattService, Bluetooth._stringToUuid(arg.characteristicUUID), android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE);
+      if (!bluetoothGattCharacteristic) {
+        reject("Could not find characteristic with UUID " + arg.characteristicUUID + " on service with UUID " + arg.serviceUUID + " on device with UUID " + arg.deviceUUID);
+        return;
+      }
+
+      var val = Bluetooth._encodeValue(arg.value);
+      if (val === null) {
+        reject("Invalid value: " + arg.value);
+        return;
+      }
+
+      bluetoothGattCharacteristic.setValue(val);
+      bluetoothGattCharacteristic.setWriteType(android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+
+      Bluetooth._connections[arg.deviceUUID].onWritePromise = resolve;
+      if (!wrapper.gatt.writeCharacteristic(bluetoothGattCharacteristic)) {
+        reject("Failed to write to characteristic " + arg.characteristicUUID);
+      }
     } catch (ex) {
       console.log("Error in Bluetooth.write: " + ex);
       reject(ex);
@@ -629,8 +698,36 @@ Bluetooth.write = function (arg) {
 Bluetooth.writeWithoutResponse = function (arg) {
   return new Promise(function (resolve, reject) {
     try {
-      // TODO implement
-      reject("writeWithoutResponse not yet implemented for Android");
+      if (!arg.value) {
+        reject("You need to provide some data to write in the 'value' property");
+        return;
+      }
+      var wrapper = Bluetooth._getWrapper(arg, reject);
+      if (wrapper === null) {
+        // no need to reject, this has already been done
+        return;
+      }
+
+      var bluetoothGattCharacteristic = Bluetooth._findCharacteristicOfType(wrapper.bluetoothGattService, Bluetooth._stringToUuid(arg.characteristicUUID), android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE);
+      if (!bluetoothGattCharacteristic) {
+        reject("Could not find characteristic with UUID " + arg.characteristicUUID + " on service with UUID " + arg.serviceUUID + " on device with UUID " + arg.deviceUUID);
+        return;
+      }
+
+      var val = Bluetooth._encodeValue(arg.value);
+      if (val === null) {
+        reject("Invalid value: " + arg.value);
+        return;
+      }
+
+      bluetoothGattCharacteristic.setValue(val);
+      bluetoothGattCharacteristic.setWriteType(android.bluetooth.BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+
+      if (wrapper.gatt.writeCharacteristic(bluetoothGattCharacteristic)) {
+        resolve();
+      } else {
+        reject("Failed to write to characteristic " + arg.characteristicUUID);
+      }
     } catch (ex) {
       console.log("Error in Bluetooth.writeWithoutResponse: " + ex);
       reject(ex);
@@ -646,7 +743,7 @@ Bluetooth.startNotifying = function (arg) {
         // no need to reject, this has already been done
         return;
       }
-      
+
       var gatt = wrapper.gatt;
       var bluetoothGattService = wrapper.bluetoothGattService;
       var characteristicUUID = Bluetooth._stringToUuid(arg.characteristicUUID);
@@ -703,7 +800,7 @@ Bluetooth.stopNotifying = function (arg) {
         // no need to reject, this has already been done
         return;
       }
-      
+
       var gatt = wrapper.gatt;
       var bluetoothGattService = wrapper.bluetoothGattService;
       var characteristicUUID = Bluetooth._stringToUuid(arg.characteristicUUID);
@@ -715,10 +812,10 @@ Bluetooth.stopNotifying = function (arg) {
         reject("Could not find characteristic with UUID " + arg.characteristicUUID + " on service with UUID " + arg.serviceUUID + " on device with UUID " + arg.deviceUUID);
         return;
       }
-      
+
       var stateObject = Bluetooth._connections[arg.deviceUUID];
       stateObject.onNotifyCallback = null;
-      
+
       if (gatt.setCharacteristicNotification(bluetoothGattCharacteristic, false)) {
         resolve();
       } else {
