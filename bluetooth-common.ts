b53c2ef97a9d75914bc79280933631e6f45bc108
George Edwards <g.edwards@gas-sense.co.uk>
2016-06-12 16:29:35 +0100

    Initial Port of Android -> Typescript

---
+++ bluetooth-common.ts
@@ -0,0 +1,28 @@
+require('./base64');
+
+export class Bluetooth {
+
+  requestCoarseLocationPermission() {
+    return new Promise(function (resolve) {
+      resolve(true);
+    });
+  };
+
+  hasCoarseLocationPermission() {
+    return new Promise(function (resolve) {
+      resolve(true);
+    });
+  };
+
+  _base64ToArrayBuffer(b64) {
+    var stringToArrayBuffer = function (str) {
+      var ret = new Uint8Array(str.length);
+      for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+      }
+      return ret.buffer;
+    };
+    return stringToArrayBuffer(atob(b64));
+  };
+
+}
