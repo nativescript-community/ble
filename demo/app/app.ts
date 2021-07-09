0e9040bb98e506148b47b4bf7fec3dbd290b10f1
Martin Guillon <martin.guillon@akylas.fr>
2021-07-09 21:32:37 +0200

    test: demo app

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,2 +1,7 @@
+
 import * as app from '@nativescript/core/application';
+import { BleTraceCategory } from '@nativescript-community/ble/bluetooth.common';
+import { Trace } from '@nativescript/core';
+Trace.addCategories(BleTraceCategory);
+Trace.enable();
 app.run({ moduleName: 'app-root' });
